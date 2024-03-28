b9e2273e4712943d63518c9198d1d8de89170261
PeterJ <35543601+BangPeter@users.noreply.github.com>
2024-03-28 10:09:00 -0400

    Updated hitbox to not hit backwards
    Changed hitbox size

--- Assets/Scripts/CollisionDetector.cs
+++ Assets/Scripts/CollisionDetector.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CollisionDetector : MonoBehaviour
 {
     [SerializeField] private int damagableLayer;
